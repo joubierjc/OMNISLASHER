870713d1e79d8b207467540b23665fca85f4c889
joubierjc <joubier.j.c@gmail.com>
2018-05-11 13:58:03 +0200

    continued skill system /2

---
+++ Assets/Scripts/Skills/PlayerModifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skill/New Player Modifier")]
+public class PlayerModifier : Skill {
+
+	public float health;
+	public int score;
+
+	public override IEnumerator CastCoroutine() {
+		var player = holder.GetComponent<Player>();
+		if(!player) {
+			yield break;
+		}
+		var hp = player.GetComponent<Health>();
+		hp.Value = Mathf.Clamp(hp.Value + health, 0f, player.maxHealth);
+		GameManager.Instance.AddScore(score);
+		HudManager.Instance.ChangeHealthDisplay(hp.Value);
+		HudManager.Instance.RefreshHealthText(hp.Value);
+		yield break;
+	}
+}
